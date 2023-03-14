cfff42d3c9fc5992bfba8c7c14e66df6c03ee327
resparx <ramprash.rspx@gmail.com>
2023-03-14 21:22:33 +0530

    landing page push

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -3,7 +3,11 @@ import React from "react";
 export const Navigation = (props) => {
   return (
     <nav id="menu" className="navbar navbar-default navbar-fixed-top">
-      <div className="container">
+      <div style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between"
+      }} className="container">
         <div className="navbar-header">
           <button
             type="button"
@@ -17,9 +21,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
             <span className="icon-bar"></span>{" "}
           </button>
-          <a className="navbar-brand page-scroll" href="#page-top">
-            React Landing Page
-          </a>{" "}
+          <img className="logo" src="https://i.imgur.com/6cvD09t.png" />
         </div>
 
         <div
@@ -28,37 +30,37 @@ export const Navigation = (props) => {
         >
           <ul className="nav navbar-nav navbar-right">
             <li>
-              <a href="#features" className="page-scroll">
+              <a href="#features" className="page-scroll nav-primary">
                 Services
               </a>
             </li>
             <li>
-              <a href="#about" className="page-scroll">
+              <a href="#about" className="page-scroll nav-primary">
                 Insights
               </a>
             </li>
             <li>
-              <a href="#services" className="page-scroll">
+              <a href="#services" className="page-scroll nav-primary">
                 About
               </a>
             </li>
             <li>
-              <a href="#portfolio" className="page-scroll">
+              <a href="#portfolio" className="page-scroll nav-primary">
                 Careers
               </a>
             </li>
             <li>
-              <a href="#testimonials" className="page-scroll">
+              <a href="#testimonials" className="page-scroll nav-primary">
                 Testimonials
               </a>
             </li>
             <li>
-              <a href="#team" className="page-scroll">
+              <a href="#team" className="page-scroll nav-primary">
                 Investments
               </a>
             </li>
             <li>
-              <a href="#contact" className="page-scroll">
+              <a href="#contact" className="page-scroll nav-primary">
                 Contact
               </a>
             </li>
