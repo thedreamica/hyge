ff0f7e2e5d5cf7fae6a96882ababe65e3f39b2f5
resparx <ramprash.rspx@gmail.com>
2023-03-21 09:42:08 +0530

    wip: ythird push

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -14,7 +14,10 @@ export const Contact = (props) => {
     const { name, value } = e.target;
     setState((prevState) => ({ ...prevState, [name]: value }));
   };
-  const clearState = () => setState({ ...initialState });
+  const clearState = () => {
+    setState({ ...initialState });
+    window.location.reload()
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
